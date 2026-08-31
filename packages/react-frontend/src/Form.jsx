d6378c24d0d11cd5486f04b2529b7d68b5cf27e1
Edgar Olozagaste-Olea <eolozagaste0@gmail.com>
2026-08-31 13:28:29 -0700

    feat: added Form component

---
+++ packages/react-frontend/src/Form.jsx
@@ -0,0 +1,41 @@
+// src/Form.jsx
+import React, { useState } from "react";
+
+function Form() {
+  const [person, setPerson] = useState({
+    // initially empty fields for name and job
+    name: "",
+    job: "",
+  });
+
+  function handleChange(event) {
+    const { name, value } = event.target; // ?
+    if (name === "job") {
+      setPerson({ name: person["name"], job: value }); // keeps the person name is value inside "name" field and updates "job" field with value.
+    } else {
+      setPerson({ name: value, job: person["job"] }); // vice-versa
+    }
+  }
+  return (
+    <form>
+      <label htmlFor="name">Name</label>
+      <input
+        type="text"
+        name="name"
+        id="name"
+        value={person.name} // ?
+        onChange={handleChange} // ? how/when does onChange occur
+      />
+      <label htmlFor="job">Job</label>
+      <input
+        type="text"
+        name="job"
+        id="job"
+        value={person.job}
+        onChange={handleChange} // ?
+      />
+    </form>
+  );
+}
+
+export default Form;
